e468a219fc796ba7d54d81c19ed27f3bb884ab84
Bodryakov.web <bodryakov.web@gmail.com>
2025-08-08 10:53:29 +0300

    .

--- panel.php
+++ panel.php
@@ -317,7 +317,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Админ-панель DOMLearn</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/admin.css">
     <script src="/tinymce/tinymce.min.js"></script>
     <style>
         /* Специфические стили для админ-панели */
